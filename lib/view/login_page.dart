867a9cdd3e98b76aa67bf669be93c19119f6c57b
muhammadarif <muhammad.arif5995@gmail.com>
2020-03-16 18:45:18 +0700

    arif login

--- lib/view/login_page.dart
+++ lib/view/login_page.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: Container(
+
+      ),
+    );
   }
 }
